520cd374982f3a8df62f37b128813f5dd4b33a51
letrixt <91343703+letrixt@users.noreply.github.com>
2021-11-29 08:53:49 -1000

    Add files via upload

---
+++ weird.py
@@ -0,0 +1,18 @@
+import sys
+import subprocess
+
+__hex__,__argv__ = [
+	0x08, 0x00, 0x0a,
+	0x3a, 0x30, 0x39,
+	0x41, 0x46, 
+],sys.argv[1];
+
+def __main__(_0x78,_0x79):
+	__0xfff__, = {
+		1, lambda x : exec(""),
+		2, lambda y : exec(""),
+		3, lambda z : exec(""),
+	};
+
+if __name__ == '__main__':
+	__main__(__hex__,__argv__)
